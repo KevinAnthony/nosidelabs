f1090485aeabbb5f888c06e77b737a5d8bf7629e
Kevin Anthony <kevin.s.anthony@gmail.com>
2016-08-09 14:56:22 +0000

    Added URL Rewrite

--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -2,8 +2,8 @@
 //Version 3.2
 $steamauth['apikey'] = "71DE25185EB70B6040AFCC7D6E8799D9"; // Your Steam WebAPI-Key found at http://steamcommunity.com/dev/apikey
 $steamauth['domainname'] = "http://www.nosidelabs.com/"; // The main URL of your website displayed in the login page
-$steamauth['logoutpage'] = "http://www.nosidelabs.com/index.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
-$steamauth['loginpage'] = "http://www.nosidelabs.com/steam.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['logoutpage'] = "http://www.nosidelabs.com/"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
+$steamauth['loginpage'] = "http://www.nosidelabs.com/steam/"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
 // System stuff
 if (empty($steamauth['apikey'])) {die("<div style='display: block; width: 100%; background-color: red; text-align: center;'>SteamAuth:<br>Please supply an API-Key!</div>");}
